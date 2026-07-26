ff869da9beb7196de0d4b8f8d9d85f6cd0b40a22
Valentyn <valikpro35@gmail.com>
2026-07-26 14:55:50 +0200

    Fix video filename case

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: { cssMinify: false },
   server: { host: "0.0.0.0", port: 5173, strictPort: true, },
 })
